3fad2786646818b6bb72155de9f8c9899c02e7a4
H171 <1091088708@qq.com>
2023-05-03 22:05:43 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <Test />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { ipcRenderer } from 'electron'
+import fs from 'node:fs'
+
+onMounted(() => {
+  console.log('ipcRenderer:',ipcRenderer)
+  console.log('fs:', fs)
+})
+</script>
---
+++ components/Test.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div>
+      <span>Node.js: </span>
+      <span id="node-version"></span>
+    </div>
+    <div>
+      <span>Chromium: </span>
+      <span id="chrome-version"></span>
+    </div>
+    <div>
+      <span>Electron:</span>
+      <span id="electron-version"></span>
+    </div>
+  </div>
+</template>
